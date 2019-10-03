cfbcf557a85fc467bb3c97db26d7d4896b0ce69b
btframework <btframework@gmail.com>
2019-10-04 01:24:41 +0300

    WeDoControl app

---
+++ WeDoControl/main.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WeDoControl
+{
+    partial class fmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btConnect = new System.Windows.Forms.Button();
+            this.btDisconnect = new System.Windows.Forms.Button();
+            this.laState = new System.Windows.Forms.Label();
+            this.laDeviceInformationTitle = new System.Windows.Forms.Label();
+            this.laFirmwareVersionTitle = new System.Windows.Forms.Label();
+            this.laHardwareVersionTitle = new System.Windows.Forms.Label();
+            this.laSoftwareVersionTitle = new System.Windows.Forms.Label();
+            this.laManufacturerNameTitle = new System.Windows.Forms.Label();
+            this.laFirmwareVersion = new System.Windows.Forms.Label();
+            this.laHardwareVersion = new System.Windows.Forms.Label();
+            this.laSoftwareVersion = new System.Windows.Forms.Label();
+            this.laManufacturerName = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btConnect
+            // 
+            this.btConnect.Location = new System.Drawing.Point(12, 12);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(75, 23);
+            this.btConnect.TabIndex = 0;
+            this.btConnect.Text = "Connect";
+            this.btConnect.UseVisualStyleBackColor = true;
+            this.btConnect.Click += new System.EventHandler(this.BtConnect_Click);
+            // 
+            // btDisconnect
+            // 
+            this.btDisconnect.Enabled = false;
+            this.btDisconnect.Location = new System.Drawing.Point(93, 12);
+            this.btDisconnect.Name = "btDisconnect";
+            this.btDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btDisconnect.TabIndex = 1;
+            this.btDisconnect.Text = "Disconnect";
+            this.btDisconnect.UseVisualStyleBackColor = true;
+            this.btDisconnect.Click += new System.EventHandler(this.BtDisconnect_Click);
+            // 
+            // laState
+            // 
+            this.laState.AutoSize = true;
+            this.laState.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.laState.Location = new System.Drawing.Point(174, 17);
+            this.laState.Name = "laState";
+            this.laState.Size = new System.Drawing.Size(85, 13);
+            this.laState.TabIndex = 2;
+            this.laState.Text = "Disconnected";
+            // 
+            // laDeviceInformationTitle
+            // 
+            this.laDeviceInformationTitle.AutoSize = true;
+            this.laDeviceInformationTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.laDeviceInformationTitle.Location = new System.Drawing.Point(30, 49);
+            this.laDeviceInformationTitle.Name = "laDeviceInformationTitle";
+            this.laDeviceInformationTitle.Size = new System.Drawing.Size(113, 13);
+            this.laDeviceInformationTitle.TabIndex = 3;
+            this.laDeviceInformationTitle.Text = "Device information";
+            // 
+            // laFirmwareVersionTitle
+            // 
+            this.laFirmwareVersionTitle.AutoSize = true;
+            this.laFirmwareVersionTitle.Location = new System.Drawing.Point(52, 74);
+            this.laFirmwareVersionTitle.Name = "laFirmwareVersionTitle";
+            this.laFirmwareVersionTitle.Size = new System.Drawing.Size(89, 13);
+            this.laFirmwareVersionTitle.TabIndex = 4;
+            this.laFirmwareVersionTitle.Text = "Firmware version:";
+            // 
+            // laHardwareVersionTitle
+            // 
+            this.laHardwareVersionTitle.AutoSize = true;
+            this.laHardwareVersionTitle.Location = new System.Drawing.Point(52, 98);
+            this.laHardwareVersionTitle.Name = "laHardwareVersionTitle";
+            this.laHardwareVersionTitle.Size = new System.Drawing.Size(93, 13);
+            this.laHardwareVersionTitle.TabIndex = 5;
+            this.laHardwareVersionTitle.Text = "Hardware version:";
+            // 
+            // laSoftwareVersionTitle
+            // 
+            this.laSoftwareVersionTitle.AutoSize = true;
+            this.laSoftwareVersionTitle.Location = new System.Drawing.Point(52, 122);
+            this.laSoftwareVersionTitle.Name = "laSoftwareVersionTitle";
+            this.laSoftwareVersionTitle.Size = new System.Drawing.Size(89, 13);
+            this.laSoftwareVersionTitle.TabIndex = 6;
+            this.laSoftwareVersionTitle.Text = "Software version:";
+            // 
+            // laManufacturerNameTitle
+            // 
+            this.laManufacturerNameTitle.AutoSize = true;
+            this.laManufacturerNameTitle.Location = new System.Drawing.Point(52, 146);
+            this.laManufacturerNameTitle.Name = "laManufacturerNameTitle";
+            this.laManufacturerNameTitle.Size = new System.Drawing.Size(102, 13);
+            this.laManufacturerNameTitle.TabIndex = 7;
+            this.laManufacturerNameTitle.Text = "Manufacturer name:";
+            // 
+            // laFirmwareVersion
+            // 
+            this.laFirmwareVersion.AutoSize = true;
+            this.laFirmwareVersion.Location = new System.Drawing.Point(162, 74);
+            this.laFirmwareVersion.Name = "laFirmwareVersion";
+            this.laFirmwareVersion.Size = new System.Drawing.Size(47, 13);
+            this.laFirmwareVersion.TabIndex = 8;
+            this.laFirmwareVersion.Text = "<empty>";
+            // 
+            // laHardwareVersion
+            // 
+            this.laHardwareVersion.AutoSize = true;
+            this.laHardwareVersion.Location = new System.Drawing.Point(162, 98);
+            this.laHardwareVersion.Name = "laHardwareVersion";
+            this.laHardwareVersion.Size = new System.Drawing.Size(47, 13);
+            this.laHardwareVersion.TabIndex = 9;
+            this.laHardwareVersion.Text = "<empty>";
+            // 
+            // laSoftwareVersion
+            // 
+            this.laSoftwareVersion.AutoSize = true;
+            this.laSoftwareVersion.Location = new System.Drawing.Point(162, 122);
+            this.laSoftwareVersion.Name = "laSoftwareVersion";
+            this.laSoftwareVersion.Size = new System.Drawing.Size(47, 13);
+            this.laSoftwareVersion.TabIndex = 10;
+            this.laSoftwareVersion.Text = "<empty>";
+            // 
+            // laManufacturerName
+            // 
+            this.laManufacturerName.AutoSize = true;
+            this.laManufacturerName.Location = new System.Drawing.Point(162, 146);
+            this.laManufacturerName.Name = "laManufacturerName";
+            this.laManufacturerName.Size = new System.Drawing.Size(47, 13);
+            this.laManufacturerName.TabIndex = 11;
+            this.laManufacturerName.Text = "<empty>";
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 60000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // fmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.laManufacturerName);
+            this.Controls.Add(this.laSoftwareVersion);
+            this.Controls.Add(this.laHardwareVersion);
+            this.Controls.Add(this.laFirmwareVersion);
+            this.Controls.Add(this.laManufacturerNameTitle);
+            this.Controls.Add(this.laSoftwareVersionTitle);
+            this.Controls.Add(this.laHardwareVersionTitle);
+            this.Controls.Add(this.laFirmwareVersionTitle);
+            this.Controls.Add(this.laDeviceInformationTitle);
+            this.Controls.Add(this.laState);
+            this.Controls.Add(this.btDisconnect);
+            this.Controls.Add(this.btConnect);
+            this.Name = "fmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LEGO WeDo Control";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FmMain_FormClosed);
+            this.Load += new System.EventHandler(this.FmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.Button btDisconnect;
+        private System.Windows.Forms.Label laState;
+        private System.Windows.Forms.Label laDeviceInformationTitle;
+        private System.Windows.Forms.Label laFirmwareVersionTitle;
+        private System.Windows.Forms.Label laHardwareVersionTitle;
+        private System.Windows.Forms.Label laSoftwareVersionTitle;
+        private System.Windows.Forms.Label laManufacturerNameTitle;
+        private System.Windows.Forms.Label laFirmwareVersion;
+        private System.Windows.Forms.Label laHardwareVersion;
+        private System.Windows.Forms.Label laSoftwareVersion;
+        private System.Windows.Forms.Label laManufacturerName;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ WeDoControl/main.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Windows.Forms;
+
+using wclCommon;
+using wclBluetooth;
+using WeDo;
+
+namespace WeDoControl
+{
+    public partial class fmMain : Form
+    {
+        private WeDoWatcher FWatcher;
+        private WeDoController FClient;
+        private wclBluetoothRadio FRadio;
+        private wclBluetoothManager FManager;
+
+        #region Helper functions
+        private void Cleanup()
+        {
+            laState.Text = "Disconnected";
+
+            laFirmwareVersion.Text = "<empty>";
+            laHardwareVersion.Text = "<empty>";
+            laSoftwareVersion.Text = "<empty>";
+            laManufacturerName.Text = "<empty>";
+
+            btDisconnect.Enabled = false;
+            btConnect.Enabled = true;
+
+            FRadio = null;
+
+            FManager.Close();
+        }
+        #endregion
+
+        #region Device information reading
+        private void DisplayDeviceInforValue(Label label, String Value, Int32 Res)
+        {
+            if (Res == wclErrors.WCL_E_SUCCESS)
+                label.Text = Value;
+            else
+            {
+                if (Res == wclBluetoothErrors.WCL_E_BLUETOOTH_LE_ATTRIBUTE_NOT_FOUND)
+                    label.Text = "<unspecified>";
+                else
+                    label.Text = "Read error: 0x" + Res.ToString("X8");
+            }
+        }
+
+        private void ReadDeviceInformation()
+        {
+            String Value;
+            Int32 Res = FClient.ReadFirmwareVersion(out Value);
+            DisplayDeviceInforValue(laFirmwareVersion, Value, Res);
+            Res = FClient.ReadHardwareVersion(out Value);
+            DisplayDeviceInforValue(laHardwareVersion, Value, Res);
+            Res = FClient.ReadSoftwareVersion(out Value);
+            DisplayDeviceInforValue(laSoftwareVersion, Value, Res);
+            Res = FClient.ReadManufacturerName(out Value);
+            DisplayDeviceInforValue(laManufacturerName, Value, Res);
+        }
+        #endregion
+
+        #region Device wahtcher events
+        private void WatcherDeviceFound(object Sender, long Address)
+        {
+            // Once device found we have to stop watcher and try to connect to the just found device.
+            FWatcher.Stop();
+            // Do not forget to stop the timer.
+            Timer.Stop();
+            // Now try to connect to just found device.
+            Int32 Res = FClient.Connect(FRadio, Address);
+            if (Res != wclErrors.WCL_E_SUCCESS)
+            {
+                Cleanup();
+                MessageBox.Show("Connect failed with error: 0x" + Res.ToString("X8"));
+            }
+            else
+                laState.Text = "Connecting...";
+        }
+        #endregion
+
+        #region WeDo Controller (Client) events
+        private void ClientConnected(object Sender, int Error)
+        {
+            if (Error != wclErrors.WCL_E_SUCCESS)
+            {
+                Cleanup();
+                MessageBox.Show("Connect failed with error: 0x" + Error.ToString("X8"));
+            }
+            else
+            {
+                btDisconnect.Enabled = true;
+
+                ReadDeviceInformation();
+            }
+        }
+
+        private void ClientDisconnected(object Sender, int Reason)
+        {
+            Cleanup();
+        }
+        #endregion
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Looks like device was not found. So terminate searching and release resources.
+            Timer.Stop();
+            FWatcher.Stop();
+            Cleanup();
+
+            MessageBox.Show("No WeDo device was found");
+        }
+
+        private void FmMain_Load(object sender, EventArgs e)
+        {
+            FWatcher = new WeDoWatcher();
+            FWatcher.OnDeviceFound += WatcherDeviceFound;
+
+            FClient = new WeDoController();
+            FClient.OnConnected += ClientConnected;
+            FClient.OnDisconnected += ClientDisconnected;
+
+            FManager = new wclBluetoothManager();
+
+            FRadio = null;
+        }
+
+        private void FmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            FWatcher.Stop();
+            FWatcher.Dispose(); // We must dispose the object!
+            FWatcher = null;
+
+            FClient.Disconnect();
+            FClient.Dispose(); // We must dispose the obejct!
+            FClient = null;
+
+            FManager.Close();
+            FManager = null;
+        }
+
+        private void BtConnect_Click(object sender, EventArgs e)
+        {
+            Int32 Res = FManager.Open();
+            if (Res != wclErrors.WCL_E_SUCCESS)
+                MessageBox.Show("Open BluetoothManager failed with error: 0x" + Res.ToString("X8"));
+            else
+            {
+                if (FManager.Count == 0)
+                    MessageBox.Show("No Bluetooth Hardware was found");
+                else
+                {
+                    for (Int32 i = 0; i < FManager.Count; i++)
+                    {
+                        if (FManager[i].Available)
+                        {
+                            FRadio = FManager[i];
+                            break;
+                        }
+                    }
+
+                    if (FRadio == null)
+                        MessageBox.Show("No available Bluetooth Radio found.");
+                    else
+                    {
+                        Res = FWatcher.Start(FRadio);
+                        if (Res != wclErrors.WCL_E_SUCCESS)
+                        {
+                            MessageBox.Show("Failed to scan WeDo devices: 0x" + Res.ToString("X8"));
+                            FRadio = null;
+                        }
+                        else
+                        {
+                            laState.Text = "Searching...";
+                            btConnect.Enabled = false;
+                            Timer.Start();
+                        }
+                    }
+                }
+
+                if (FRadio == null)
+                    FManager.Close();
+            }
+        }
+
+        private void BtDisconnect_Click(object sender, EventArgs e)
+        {
+            FClient.Disconnect();
+        }
+
+        public fmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
