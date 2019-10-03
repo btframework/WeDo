78f0eacdcaf5f9c468c9d7eeaf38e08eea8c2d0c
btframework <btframework@gmail.com>
2019-10-04 01:35:13 +0300

    Removed WeDo docs

--- Docs/LegoDeviceSDK/Interfaces/NamespaceDoc.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LegoDeviceSDK.Interfaces {
-	/// <summary>
-	/// Namespace for all Interfaces and Delegates.
-	/// </summary>
-	[System.Runtime.CompilerServices.CompilerGenerated]
-	class NamespaceDoc {
-	}
-}
